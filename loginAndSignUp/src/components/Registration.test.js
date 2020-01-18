e38e7b4a52c1dd2f995d28f5b29a9f947e8e4ab0
Umair Mustafa <umairhassaaan@gmail.com>
2020-01-18 10:30:57 +0500

    latest

--- loginAndSignUp/src/components/Registration.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Registration from './Registration';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Registration />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
